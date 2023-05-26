145beba96dc9cf1dc47ea72b278a1f207d5ee779
abidcf <abid@cloudfort.in>
2023-05-26 08:54:25 +0530

    Destroyed the connection after done uploading

--- client.js
+++ client.js
@@ -1,8 +1,23 @@
 const net = require("net");
+const fs = require("fs/promises");
 
 const HOST = "::1";
 const PORT = 4002;
 
 const socket = net.createConnection({ host: HOST, port: PORT }, async () => {
   console.log("Connected to server");
+
+  const fileReadHandle = await fs.open(`file-to-be-read.txt`, "r");
+  const fileStream = fileReadHandle.createReadStream();
+
+  fileStream.on("data", (data) => {
+    socket.write(data);
+  });
+
+  fileStream.on("end", () => {
+    console.log("File uploaded successfully!");
+    socket.end();
+  });
+
+  //   socket.
 });
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const net = require("net");
+const fs = require("fs/promises");
 
 const server = net.createServer((socket) => {});
 
-server.on("connection", () => {
+server.on("connection", async (socket) => {
   console.log("New connection!");
+
+  const fileWriteHandle = await fs.open(`storage/myfile.txt`, "w");
+  const fileStream = fileWriteHandle.createWriteStream();
+
+  socket.on("data", (data) => {
+    fileStream.write(data);
+  });
+
+  socket.on("end", () => {
+    fileStream.close();
+    fileWriteHandle.close();
+  });
 });
 
 server.listen(4002, "::1", () => {
