7a43dd9ace401fdf443392391d60a59297cd557d
abidcf <abid@cloudfort.in>
2023-05-27 18:22:16 +0530

    Upload progress added

--- client.js
+++ client.js
@@ -5,6 +5,22 @@ const path = require("path");
 const HOST = "::1";
 const PORT = 4002;
 
+const clearLine = (dir) => {
+  return new Promise((resolve) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+
 const socket = net.createConnection({ host: HOST, port: PORT }, async () => {
   const filePath = process.argv[2];
   console.log(filePath);
@@ -22,11 +38,27 @@ const socket = net.createConnection({ host: HOST, port: PORT }, async () => {
   const fileReadHandle = await fs.open(filePath, "r");
   const fileStream = fileReadHandle.createReadStream();
 
-  fileStream.on("data", (data) => {
+  const fileSize = (await fileReadHandle.stat()).size;
+  let bytesUploaded = 0;
+  let percentUploaded = 0;
+
+  fileStream.on("data", async (data) => {
     const canWriteMore = socket.write(data);
 
     if (!canWriteMore) {
       fileStream.pause();
+    }
+
+    bytesUploaded += data.length;
+
+    let newPercentUploaded = Math.floor((bytesUploaded / fileSize) * 100);
+
+    if (newPercentUploaded !== percentUploaded) {
+      console.log(`Uploaded... ${percentUploaded}%`);
+      await moveCursor(0, -1);
+      await clearLine(0);
+
+      percentUploaded = newPercentUploaded;
     }
   });
 
